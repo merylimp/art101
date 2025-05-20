2bf455fe028b711e3f7520ea16845dda977949b0
merylimp <merylimperial@gmail.com>
2025-05-20 16:16:09 -0700

    stuff

---
+++ lab12/js/lab.js
@@ -0,0 +1,21 @@
+/*
+   lab.js - creating a function dependent on conditionals
+
+   Requirements: functions must be functional lol 
+
+   Author: Meryl Cabiong
+   Date: May 20, 2025
+*/
+
+
+// given a numver, return a 1d mansion
+
+function getHouse(num) {
+    // get the remainder when divided by 4
+    let remainder = num % 4;
+    let str = "";
+    // assign mansions with descriptions based on the remainder
+    if (remainder == 0) {
+        str = "House of Harry: Known for "
+    }
+}
